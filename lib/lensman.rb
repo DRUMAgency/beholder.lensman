7df15257ad35df135eecabc053ee88ab659bc40a
David Briggs <david.briggs@drumagency.com>
2018-05-21 14:18:22 -0400

    add faraday middleware

--- lib/lensman.rb
+++ lib/lensman.rb
@@ -1,12 +1,13 @@
 require "lensman/version"
 require "lensman/configuration"
-require "faraday"
+require 'faraday'
+require 'faraday_middleware'
 
 module Lensman
   def self.record(type, identity, details)
     conn = Faraday.new(url: Lensman.configuration.url) do |c|
       c.token_auth(Lensman.configuration.api_key)
-      c.request :url_encode
+      c.request :json
       c.adapter Faraday.default_adapter
     end
 
